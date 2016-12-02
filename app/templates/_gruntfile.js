43daacee9a00c8d61a04f83e8bd8fb2ec5de9df7
Christian Schramm <schramm@webit.de>
2016-12-02 10:06:32 +0100

    [TASK] line-endings and tabs

--- app/templates/_gruntfile.js
+++ app/templates/_gruntfile.js
@@ -310,33 +310,33 @@ module.exports = function(grunt) {
           }
         ]
       },
-      <% if (livereload) { %>
-        dev: {
-          options: {
-            excludeBuiltins: true,
-            patterns: [
-              {
-                match: /<\/body>/g,
-                replacement: function (match, type, file) {
-                  var
-                  body_markup = '</body>',
-                  livereload_markup = '<script>document.write(\'<script src="http://\' + (location.host || \'localhost\').split(\':\')[0] + \':35729/livereload.js?snipver=1"></\' + \'script>\')</script>';
-
-                  return livereload_markup +'\n\n' + body_markup + '\n';
-                }
-              },
-            ]
-          },
-          files: [
+    <% if (livereload) { %>
+      dev: {
+        options: {
+          excludeBuiltins: true,
+          patterns: [
             {
-              expand: true,
-              flatten: true,
-              src: ['build/*.html'],
-              dest: 'build/'
-            }
+              match: /<\/body>/g,
+              replacement: function (match, type, file) {
+                var
+                body_markup = '</body>',
+                livereload_markup = '<script>document.write(\'<script src="http://\' + (location.host || \'localhost\').split(\':\')[0] + \':35729/livereload.js?snipver=1"></\' + \'script>\')</script>';
+
+                return livereload_markup +'\n\n' + body_markup + '\n';
+              }
+            },
           ]
-        }
-        <% } %>
+        },
+        files: [
+          {
+            expand: true,
+            flatten: true,
+            src: ['build/*.html'],
+            dest: 'build/'
+          }
+        ]
+      }
+    <% } %>
       },
 
     requirejs: {
